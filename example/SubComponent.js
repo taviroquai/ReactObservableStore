40a2f231d8102983c6046390742392efc9121636
Marco Afonso <mafonso333@gmail.com>
2018-10-02 00:51:45 +0100

    Watch react 1.5 component lifecycle

--- example/SubComponent.js
+++ example/SubComponent.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import assign from 'lodash-es/assign';
 import { updateSync, updateAsync } from './actions';
 import Store from '../lib';
 import { Link } from 'react-router-dom';
 
 class SubComponent extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		// Test with local state
+		this.state = {
+			title: 'local state',
+			gDSfP: 'not yet',
+			sCU: 'not yet',
+			CdM: 'not yet',
+			CdU: 'not yet',
+			alldone: 'not yet'
+		};
+	}
+
+	static getDerivedStateFromProps(props, state) {
+		let nextState = assign({}, state);
+		nextState.gDSfP = 'done';
+		return nextState;
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextState.alldone === 'not yet';
+	}
+
+	componentDidMount() {
+		setTimeout(() => {
+			this.setState((state, props) => {
+				let nextState = assign({}, state);
+				nextState.CdM = 'done';
+				return nextState;
+			});
+		}, 1500);
+	}
+
+	componentDidUpdate() {
+		setTimeout(() => {
+			this.setState((state, props) => {
+				let nextState = assign({}, state);
+				nextState.CdU = 'done';
+				nextState.title = state.CdU === 'done' ? props.title : state.title;
+				nextState.alldone = state.title === props.title ? 'yes!' : state.alldone;
+				return nextState;
+			});
+		}, 1500);
+	}
+
+	updateSync(value) {
+		this.setState((state, props) => {
+			return {
+				title: 'reset',
+				gDSfP: 'not yet',
+				sCU: 'not yet',
+				CdM: state.CdM,
+				CdU: 'not yet',
+				alldone: 'not yet'
+			};
+		}, () => {
+			updateSync(value);
+		});
+	}
+	
  	render () {
 		const { title } = this.props;
 		return (
@@ -18,10 +81,16 @@ class SubComponent extends React.Component {
 					React Router Link example: <Link to="/record/id">click</Link>
 				</p>
         <p>
-					Button <button onClick={(e) => updateSync('sync') }>Update</button> example
+					Button <button onClick={(e) => this.updateSync('sync') }>Update without unmount</button> example
 				</p>
 				<p>
-					Async <button onClick={(e) => updateAsync('async') }>Update</button> example
+					Async <button onClick={(e) => updateAsync('async') }>Update with unmout</button> example
+				</p>
+				<p>
+					getDerivedStateFromProps: {this.state.gDSfP}<br />
+					componentDidMount: {this.state.CdM}<br />
+					componentDidUpdate: {this.state.CdU}<br />
+					Local state <em>this.state.title</em>: {this.state.title}
 				</p>
 			</div>
 		);
